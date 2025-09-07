0fdb787f9b54fba8dd2d1b1e15b978c184e2d05e
Daniel Suruagy <daniel.suruagy@outlook.com>
2025-09-07 20:59:58 -0300

    Ajustes Básicos

---
+++ Models/Reserva.cs
@@ -0,0 +1,53 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; private set; } = new List<Pessoa>();
+        public Suite Suite { get; private set; }
+        public int DiasReservados { get; private set; }
+        
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new InvalidOperationException("A capacidade da suíte é menor que o número de hóspedes.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+        if (Hospedes != null)
+                return Hospedes.Count;
+
+        return 0;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.9m;
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+ ## 📌 Desafio: Projeto de Hospedagem
+
+Este projeto simula um sistema de hospedagem em C#, onde é possível cadastrar hóspedes, associá-los a uma suíte e calcular o valor da estadia com base nos dias reservados.
+
+## 🚀 Funcionalidades
+- **Cadastrar hóspedes em uma reserva.**  
+- **Associar uma suíte à reserva.**
+- **Verificar a quantidade de hóspedes cadastrados.**
+- **Calcular o valor total da diária, aplicando desconto de 10% para estadias de 10 dias ou mais.**
+- **Garantir que o número de hóspedes não ultrapasse a capacidade da suíte.**
